f289d7a748eb2cd084beaacd4bcb79217ec9b105
tuan-minh <tuanminh20194334@gmail.com>
2023-02-28 11:15:21 +0700

    ss

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instanceAxios = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'http://localhost:3001/api',
   timeout: 300000,
   cache: 'no-cache'
 });
