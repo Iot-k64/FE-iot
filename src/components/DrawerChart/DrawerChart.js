0228d903edae66848fa0ee61c01e6c976297badb
dattm <manhdat.tang@secomus.com>
2023-03-04 22:12:03 +0700

    feat real-time stat

--- src/components/DrawerChart/DrawerChart.js
+++ src/components/DrawerChart/DrawerChart.js
@@ -1,66 +1,75 @@
-import {
-  useQuery,
-  useQueryClient
-} from '@tanstack/react-query';
-import { Button, Drawer, message } from 'antd';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button, Drawer, message, Spin } from 'antd';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
-import React from 'react';
+import React, { useState } from 'react';
 import TrackRecord from '../../api/trackRecord';
+import { LoadingOutlined } from '@ant-design/icons';
 
 export default function DrawerChart({
   showDrawer,
   closeDrawer,
   dataContainer
 }) {
+  const [dataTemp, setDateTemp] = useState([]);
   const dataTrackRecords = useQuery(
-    ['getTrackRecords'],
+    ['getTrackRecords', dataContainer._id],
     () => TrackRecord.getTrackRecord(dataContainer._id),
     {
-      keepPreviousData: true
+      keepPreviousData: true,
+      refetchInterval: 5000
     }
   );
   const queryClient = useQueryClient();
-
-  console.log(dataTrackRecords.data)
+  console.log(dataTemp);
   const getDataHighChart = (isTemp) => {
-    const datas = [];
-    const datasStandard = [];
-    if(isTemp) {
-      dataTrackRecords.data?.forEach(trackRecord => {
-        datas.push({y: trackRecord.temperature, x: (new Date(trackRecord.createdAt)).getTime()})
-        datasStandard.push({y: dataContainer.product?.standardTemp, x: (new Date(trackRecord.createdAt)).getTime()})
-      });
-      const xxx = [
+    if (isTemp) {
+      return [
         {
           name: 'Current Temperature',
-          data: datas
+          data:
+            !dataTrackRecords.isLoading &&
+            dataTrackRecords.data
+              .map((item) => item.temperature)
+              .reverse()
+        },
+        {
+          name: 'Standard temperature',
+          data:
+            !dataTrackRecords.isLoading &&
+            dataTrackRecords.data.map((item) => 30)
         }
-      ]
-      console.log(xxx)
-
-      return xxx
+      ];
+    } else {
+      return [
+        {
+          name: 'Current Humidity',
+          data:
+            !dataTrackRecords.isLoading &&
+            dataTrackRecords.data
+              .map((item) => item.humidity)
+              .reverse()
+        },
+        {
+          name: 'Standard humidity',
+          data:
+            !dataTrackRecords.isLoading &&
+            dataTrackRecords.data.map((item) => 25)
+        }
+      ];
     }
-    dataTrackRecords.data?.forEach(trackRecord => {
-      datas.push({y: trackRecord.humidity, x: (new Date(trackRecord.createdAt)).getTime()})
-      datasStandard.push({y: dataContainer.product?.standardHumi, x: (new Date(trackRecord.createdAt)).getTime()})
-    });
-    return [
-      {
-        name: 'Current Humidity',
-        data: datas
-      },
-      {
-        name: 'Standard Humidity',
-        data: datasStandard
-      }
-    ]
-  }
+  };
   const handleTroubleshooting = () => {
     setTimeout(() => {
       message.success('Successful adjustment');
     }, 3000);
   };
+
+  if (dataTrackRecords.isLoading) {
+    const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+    <Spin indicator={antIcon} />;
+  }
+
   return (
     <Drawer
       title="Basic Drawer"
@@ -71,137 +80,116 @@ export default function DrawerChart({
     >
       <HighchartsReact
         highcharts={Highcharts}
-        // options={{
-        //   title: {
-        //     text: 'Container 1',
-        //     align: 'left'
-        //   },
+        options={{
+          title: {
+            text: 'Temperature',
+            align: 'left'
+          },
 
-        //   subtitle: {
-        //     text:
-        //       'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
-        //     align: 'left'
-        //   },
-
-        //   yAxis: {
-        //     title: {
-        //       text: 'Recorded Temperature (C°)'
-        //     }
-        //   },
-
-        //   xAxis: {
-        //     accessibility: {
-        //       rangeDescription: 'Time'
-        //     }
-        //   },
-
-        //   legend: {
-        //     layout: 'vertical',
-        //     align: 'right',
-        //     verticalAlign: 'middle'
-        //   },
-
-        //   plotOptions: {
-        //     series: {
-        //       label: {
-        //         connectorAllowed: false
-        //       },
-        //       pointStart: 2010
-        //     }
-        //   },
-
-        //   series: getDataHighChart(true),
-
-        //   responsive: {
-        //     rules: [
-        //       {
-        //         condition: {
-        //           maxWidth: 500
-        //         },
-        //         chartOptions: {
-        //           legend: {
-        //             layout: 'horizontal',
-        //             align: 'center',
-        //             verticalAlign: 'bottom'
-        //           }
-        //         }
-        //       }
-        //     ]
-        //   }
-        // }}
-        options={{ 
-          chart: {
-            type: 'spline',
-            animation: Highcharts.svg, // don't animate in old IE
-            marginRight: 10,
-            events: {
-                load: function () {
-    
-                    // set up the updating of the chart each second
-                    var series = this.series[0];
-
-                    setInterval(function () {
-                        var x = (new Date()).getTime(), // current time
-                            y = Math.random()*20;
-                        series.addPoint([x, y], true, true);
-                        // series1.addPoint([x, dataContainer.product.standardTemp], true, true);
-
-                    }, 5000);
-                }
-            }
-        },
-    
-        time: {
-            useUTC: false
-        },
-    
-        title: {
-            text: 'Live random data'
-        },
-    
-        accessibility: {
-            announceNewData: {
-                enabled: true,
-                minAnnounceInterval: 15000,
-                announcementFormatter: function (allSeries, newSeries, newPoint) {
-                    if (newPoint) {
-                        return 'New point added. Value: ' + newPoint.y;
-                    }
-                    return false;
-                }
-            }
-        },
-    
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150
-        },
-    
-        yAxis: {
+          yAxis: {
             title: {
-                text: 'Value'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
-        },
-    
-        tooltip: {
-            headerFormat: '<b>{series.name}</b><br/>',
-            pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-        },
-    
-        legend: {
-            enabled: false
-        },
-    
-        exporting: {
-            enabled: false
-        },
-    
-        series: getDataHighChart(true)
+              text: 'Recorded Temperature (C°)'
+            }
+          },
+
+          xAxis: {
+            accessibility: {
+              rangeDescription: 'Time'
+            }
+          },
+
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+          },
+
+          plotOptions: {
+            series: {
+              label: {
+                connectorAllowed: false
+              },
+              pointStart: 2010
+            }
+          },
+
+          series: getDataHighChart(true),
+
+          responsive: {
+            rules: [
+              {
+                condition: {
+                  maxWidth: 500
+                },
+                chartOptions: {
+                  legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                  }
+                }
+              }
+            ]
+          }
+        }}
+      />
+      <Button onClick={handleTroubleshooting} danger>
+        Troubleshoot
+      </Button>
+      <div style={{ margin: '8px 0' }}></div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={{
+          title: {
+            text: 'Humidity',
+            align: 'left'
+          },
+
+          yAxis: {
+            title: {
+              text: 'Recorded Humidity '
+            }
+          },
+
+          xAxis: {
+            accessibility: {
+              rangeDescription: 'Time'
+            }
+          },
+
+          legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+          },
+
+          plotOptions: {
+            series: {
+              label: {
+                connectorAllowed: false
+              },
+              pointStart: 1
+            }
+          },
+
+          series: getDataHighChart(false),
+
+          responsive: {
+            rules: [
+              {
+                condition: {
+                  maxWidth: 500
+                },
+                chartOptions: {
+                  legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom'
+                  }
+                }
+              }
+            ]
+          }
         }}
       />
       <Button onClick={handleTroubleshooting} danger>
